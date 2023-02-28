ff1a675d0886ed5c06c7396c5e89ff53200edb88
Kene Aniekwena <kc.boys@live.com>
2023-02-28 12:31:16 +0100

    Update [unit].tsx

--- app/polling-unit/[unit].tsx
+++ app/polling-unit/[unit].tsx
@@ -1,64 +1,87 @@
-import { View, Text, StyleSheet, Pressable, Image } from 'react-native'
-import React from 'react'
-import { Link, Stack, useSearchParams } from 'expo-router'
-import useFetch from '../../hooks/useFetch';
-import { useFilePicker } from 'use-file-picker';
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
+import React from "react";
+import { Link, Stack, useSearchParams } from "expo-router";
+import useFetch from "../../hooks/useFetch";
+import { useFilePicker } from "use-file-picker";
 
 export default function state() {
+  const { unit } = useSearchParams();
+  const { data } = useFetch("/api/pu?pu=" + unit);
 
-    const { unit } = useSearchParams();
-    const { data, } = useFetch('/api/pu?pu=' + unit)
+  const [openFileSelector, { filesContent, loading, errors }] = useFilePicker({
+    readAs: "DataURL",
+    accept: "image/*",
+    multiple: false,
+    limitFilesConfig: { max: 1 },
+    maxFileSize: 10,
+    imageSizeRestrictions: {
+      maxHeight: 1600, // in pixels
+      maxWidth: 1600,
+    },
+  });
 
-    const [openFileSelector, { filesContent, loading, errors }] = useFilePicker({
-        readAs: 'DataURL',
-        accept: 'image/*',
-        multiple: false,
-        limitFilesConfig: { max: 1 },
-        maxFileSize: 10,
-        imageSizeRestrictions: {
-            maxHeight: 1600, // in pixels
-            maxWidth: 1600,
-        },
+  const uploadResults = async () => {
+    const formData = new FormData();
+    const file = await (await fetch(filesContent[0].content)).blob();
+    formData.append("file", file);
+    formData.append("pu", unit);
+
+    const response = await fetch("/uploads", {
+      method: "POST",
+      body: formData,
     });
+  };
 
-    const uploadResults = async () => {
-        const formData = new FormData();
-        const file = await (await fetch(filesContent[0].content)).blob();
-        formData.append('file', file);
-        formData.append('pu', unit);
+  return (
+    <View
+      style={{
+        backgroundColor: "black",
+        paddingHorizontal: 15,
+        flex: 1,
+        marginBottom: 15,
+      }}
+    >
+      <Stack.Screen options={{ title: `${data?.pu ?? ""} Polling Unit` }} />
 
-        const response = await fetch('/uploads', {
-            method: 'POST',
-            body: formData,
-        });
-    }
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-end",
+        }}
+      >
+        <Pressable
+          style={{
+            backgroundColor: "red",
+            padding: 15,
+            borderRadius: 5,
+            borderWidth: 1,
+          }}
+          onPress={openFileSelector}
+        >
+          <Text style={{ color: "white", fontSize: 18, textAlign: "center" }}>
+            Upload Result
+          </Text>
+        </Pressable>
+      </View>
 
-    return (
-        <View style={{ backgroundColor: "black", paddingBottom: 50 }}>
-            <Stack.Screen options={{ title: `${data?.pu ?? ""} Polling Unit` }} />
-
-            <Pressable onPress={openFileSelector} style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-            }}>
-                <Text style={{ color: "white", fontSize: 24 }}>Upload Result</Text>
-            </Pressable>
-
-            {
-                filesContent[0] && (
-                    <View>
-                        <Image source={{ uri: filesContent[0].content }} style={{ width: 200, height: 200 }} />
-                        <Pressable onPress={uploadResults} style={{
-                            flexDirection: "row",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                        }}>
-                            <Text style={{ color: "white", fontSize: 24 }}>Upload</Text>
-                        </Pressable>
-
-                    </View>)
-            }
+      {filesContent[0] && (
+        <View>
+          <Image
+            source={{ uri: filesContent[0].content }}
+            style={{ width: 200, height: 200 }}
+          />
+          <Pressable
+            onPress={uploadResults}
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 24 }}>Upload</Text>
+          </Pressable>
         </View>
-    )
+      )}
+    </View>
+  );
 }
